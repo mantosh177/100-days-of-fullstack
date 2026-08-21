2cf6078c91e3577c722cc3c01775cf8b6f37d3e9
mantosh <kumarmantosh58989@gmail.com>
2026-08-21 23:19:53 +0530

    completed data types and all types of operators in javascript

--- TAILWIND/day-16-Javascript-started/hello.js
+++ TAILWIND/day-16-Javascript-started/hello.js
@@ -1,1 +1,43 @@
-console.log("Hello world")
+// const { CURSOR_FLAGS } = require("mongodb");
+
+// console.log("Hello world");
+// console.log("Hello world");
+
+
+// console.table({city: "Jaipur"})
+
+// console.log("mantosh" ) /*another way to print something*/
+
+// Variable
+
+var score = 103;
+//  or we can use 
+
+let marks = 39;
+let name = "mantosh"
+let isLoggedin =false;
+
+
+// Object
+let teaTypes = ['Lemon tea', 'orange',9];
+let user = {firstName: "mantosh", LastName : "kumar", num : 99};
+
+let getScore =score;
+score= 99;
+
+console.log(score, teaTypes ,marks, isLoggedin,name , user);
+
+
+/*
+DATA TYPES
+
+1. String
+2. Number
+3. Boolean(yes or no )
+4. Bigint
+5. Undefined
+6. null
+7. Objects
+8. Symbol
+9. 
+*/
---
+++ TAILWIND/day17/operators.js
@@ -0,0 +1,80 @@
+
+
+// Addition
+let add = 4 + 8;
+let sccore = 193;
+let bonus = 44;
+
+let total = sccore + bonus;
+
+console.log(add , total);
+
+// Subtract
+
+let sub = 9 - 3;
+console.log(sub);
+
+// Multiplication
+
+let mult = 3 * 5;
+console.log(mult);
+
+// Division 
+
+let divi = 8 / 2;
+console.log(divi);
+
+// Reamainder
+
+let Reamainder = 9 % 2;
+console.log(Reamainder)
+
+// Power
+let Power = 2 ** 4;
+console.log(Power);
+
+// Increament operator 
+
+let myscore = 100;
+myscore++;
+console.log(myscore);
+
+// Decreament operator 
+
+let credits = 56;
+credits-- 
+console.log(credits);
+
+
+// Comparison operator 
+let num1 = 3;
+let num2 = 3;
+let num3 = 5;
+
+console.log(num1 == num2);
+console.log(num1 == num3);
+console.log(num1 != num3);
+console.log(num1 < num3);
+
+
+// // Logical operator 
+// &&  <-- and operator 
+// || <-- or operator
+// ! <-- reverse operator
+
+let isLoggedin = true;
+let isPaid = false;
+console.log(isLoggedin && isPaid);
+console.log(isLoggedin || isPaid);
+console.log( ! isLoggedin);
+
+// Assignement operator 
+
+let score = 10 + 12;
+console.log(score);
+
+let num = 10;
+num += 5;
+console.log(num);
+
+
