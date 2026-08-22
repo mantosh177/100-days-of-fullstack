b6af88a00c2c4e91a1cf4ea7a3aaf3da95dc07ba
mantosh <kumarmantosh58989@gmail.com>
2026-08-22 23:31:17 +0530

    learned privitives data types

---
+++ TAILWIND/day18/Primitives.js
@@ -0,0 +1,48 @@
+
+// Numbers
+
+// let balance = 120;
+// let anotherBalance = new Number(120);
+
+// console.log(anotherBalance);
+// console.log(balance);
+// console.log(typeof(anotherBalance));
+// console.log(anotherBalance.valueOf())
+// console.log(typeof(balance));
+
+
+// null and undefined 
+
+// let firstName;  --> it is undefined 
+// let firstName = null;
+
+// console.log(firstName);
+
+
+// // String
+
+// let myString = "hello";
+// let myString1 = 'Holaa';
+// let firstName1 = 'mantosh';
+
+// let oldGreet = myString + " kaise ho " + " mantosh";
+// console.log(oldGreet);
+
+// let greetMessage = `ram ram mittr ${firstName1}!`;
+// let demoOne = `Value is ${2 * 2}`;
+// console.log(demoOne);
+// console.log(greetMessage)
+
+
+//  symbol
+
+let sm1 = Symbol();
+let sm2 = Symbol();
+
+console.log(sm1 == sm2);
+
+let sm3 = Symbol("mantosh");
+let sm4 = Symbol("mantosh");
+
+console.log(sm3 == sm4);
+
