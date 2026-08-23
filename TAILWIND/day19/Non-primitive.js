82b27ea7feb7c9fa14e71d7834f0d26e818938a6
mantosh <kumarmantosh58989@gmail.com>
2026-08-23 22:09:38 +0530

    learned some functions and objects

---
+++ TAILWIND/day19/Non-primitive.js
@@ -0,0 +1,22 @@
+
+
+// const username = {
+//     firstname: "mantosh",
+//     isLoggedin:true,
+//     "ram ram" : "jai shree ram "
+    
+// };
+// username.firstname = "Mr. M";
+// username.lastname = "kumar";
+
+
+// console.log(username);
+// console.log(username.firstname);
+// console.log(username.lastname);
+// console.log(typeof(username));
+// console.log(username["ram ram "]);   /*you can use this also it is important */
+
+
+
+let today  = new Date();
+console.log(today.getDate())
