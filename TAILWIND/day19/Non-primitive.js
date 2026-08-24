1c949c19adf2a666ccbe2639833b3a55cadf8b7e
mantosh <kumarmantosh58989@gmail.com>
2026-08-24 23:10:46 +0530

    completed all Non-primitive data types

--- TAILWIND/day19/Non-primitive.js
+++ TAILWIND/day19/Non-primitive.js
@@ -18,5 +18,22 @@
 
 
 
-let today  = new Date();
-console.log(today.getDate())
+// let today  = new Date();
+// console.log(today.getDate())
+
+// Array
+
+// let hero =["a", "b" ,"c" , true];
+// let anotheruser = ["mantosh", "kumar" , "isLoggedin"];
+
+
+// // console.log(anotheruser[0]);
+
+// let isValue = true;
+// console.log((isValue) + 1);
+// console.log(1 +"1");
+
+let isNum = "2abc";
+console.log(typeof(isNum))
+console.log(typeof Number(isNum));
+
