4c311666184afec19e2f87e3cc825cd222989ff4
mantosh <kumarmantosh58989@gmail.com>
2026-08-25 21:45:11 +0530

    completed conditions and solved 5 problems of conditions

---
+++ TAILWIND/day20/Conditionals.js
@@ -0,0 +1,53 @@
+// condition challenges
+
+// let num1 = 5;
+// let num2 = 8;
+// console.log(" I am regular upper code");
+// if(num1 < num2) {
+//     console.log("num 1 is greater than num2");
+// } else  {
+// console.log("Nope, num1 is not greater ")
+// }
+// console.log("I am regular bottom code");
+
+
+// let username = "chai"
+// let anotheruser = "chai"
+
+// if (username == anotheruser){
+//     console.log("Pick another username");
+// } else{
+//     console.log("you can pick this username ");
+// }
+
+// Checking if a variable is number or not 
+
+// let score = 99;
+
+// if(typeof score === 'number'){
+//     console.log("this is a number ")
+// } else{
+//     console.log("not a number ")
+// }
+
+// Checking if a boolean value is true or false;
+
+// let isTeaReady = false;
+
+// if(isTeaReady){
+//     console.log("Tea is ready ")
+// } else{
+//     console.log("tea is not ready ")
+// }
+
+// Checking if an array is empty or not ;
+
+let items = [9];
+
+console.log(items.length);
+
+if(items.length===0){
+    console.log("empty")
+} else{
+    console.log("array is not empty ");
+}
