43d4c7e47d3c52b8975cb82967b1946a42052d1d
mantosh <kumarmantosh58989@gmail.com>
2026-08-28 21:50:21 +0530

    defeated array problems

--- TAILWIND/day21/10_problems_array.js
+++ TAILWIND/day21/10_problems_array.js
@@ -1,5 +1,20 @@
+// 1.m create an array named teaDlavors and it should have content "green tea", "black tea", "oolong tea" . 
+    //    make a new variable and acces and store the first element of teaFlavors array. 
 
+// let teaFlavors = ["green tea", "black tea", "oolong tea"];
 
-let teaFlavors = ["green tea", "black tea", "oolong tea"];
+// const firstTea = teaFlavors[0]
 
-console.log(teaFlavors[0]);
+// 2.
+
+let cities = ["London", "Tokyo", "Parish","New York "];
+
+const favoritecity = cities[2];
+
+// 3. You have an array named 'teaTypes' containing "herbal tea ". "White tea", "masala chai ", 
+        // Change the second element of the array to "jasmine tea".
+
+        let teaTypes = ["herbal tea", "white tea", "masala chai"];
+        teaTypes[1] = "jasmine tea"
+        console.log(teaTypes)
+
--- TAILWIND/day22-loopJS/loops.js
+++ TAILWIND/day22-loopJS/loops.js
@@ -10,3 +10,19 @@ while (i <= 5 ) {
     i++;
 }
 console.log(sum);
+
+/* 2. Write a "for" loop that loops through the array ["green tea", "black tea", "Masala chai", "oolong tea"] 
+and stops the loop when it finds "masala chai".*/
+
+let tea = ["green tea", "black tea", "masala chai", "oolong tea"];
+
+let selectedTeas = [];
+
+for(let t = 0; i< tea.length; i++){
+    if(tea[i]=== 'masala chai'){
+        break;
+    }
+    selectedTeas.push(tea[i])
+
+}
+console.log(selectedTeas)
