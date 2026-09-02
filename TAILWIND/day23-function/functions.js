708abd2a6d8e9f32cc9677d93ef4aa1b8d6a9df8
mantosh <kumarmantosh58989@gmail.com>
2026-09-02 21:22:11 +0530

    faced 3 problems of functions  like adding numbers and string and also learned about promt which is a browser api and we cant use it with node js

--- TAILWIND/day23-function/functions.js
+++ TAILWIND/day23-function/functions.js
@@ -1,13 +1,57 @@
 // A function is a reusable block of code designed to perform a specific task. It runs when we call/invoke it.
 
 
-function add() {
-    console.log(2 + 3);
+// function add() {
+//     console.log(2 + 3);
+// }
+
+// add();
+// function greet() {
+//     console.log("Hello, Mantosh!");
+// }
+
+// greet();
+
+
+// function name(){
+//     console.log("Mantosh kuamar , RAM Ram ");
+
+// }
+// // name();
+
+// function greet(name){
+//     console.log("Hello "+ name)
+// }
+// greet("Mantosh");
+
+
+// function add(a , b){
+//     return(a + b );
+
+// }
+
+// let result = add( 32 , 5 );
+// console.log(result)
+
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function add(a, b) {
+    return a + b;
 }
 
-add();
-function greet() {
-    console.log("Hello, Mantosh!");
-}
+rl.question("Enter first number: ", (a) => {
+    rl.question("Enter second number: ", (b) => {
 
-greet();
+        let result = add(Number(a), Number(b));
+
+        console.log("Result:", result);
+
+        rl.close();
+    });
+});
