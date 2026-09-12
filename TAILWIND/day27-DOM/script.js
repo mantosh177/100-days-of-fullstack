875656bd516f187fb5607b4cf062b8712d8de42a
mantosh <kumarmantosh58989@gmail.com>
2026-09-12 23:17:23 +0530

    learned  8 dom manipulation ways

--- TAILWIND/day27-DOM/script.js
+++ TAILWIND/day27-DOM/script.js
@@ -1,6 +1,10 @@
 // example 1
 
-const { Double } = require("mongodb");
+// const { Double } = require("mongodb");
+
+// const { Double } = require("mongodb");
+
+
 
 document
 .getElementById("changeTextButton")
@@ -22,13 +26,56 @@ document
 
 // example: 3
 
-// document
-// .getElementByIdA("changeOrder")
-// .addEventListener("click", function (){
-//     let coffeeType = document.getElementById("coffeeType");
-//     coffeeType.textContent = "Espresso";
-//     // coffeeType.style.backgroundColor = "red"
+document
+.getElementById("changeOrder")
+.addEventListener("click", function(){
+    let coffee = document.getElementById("coffeeOrder");
+    coffee.textContent = "Espresso";
+    coffee.style.backgroundColor = "brown"
+    coffee.style.padding = "5px";
+})
 
+
+
+// Example: 4
+
+document
+.getElementById("addNewItem")
+.addEventListener("click", function(){
+
+    let newItem = document.createElement('li')
+    newItem.textContent = "Eggs"
+
+    document.getElementById("shoppingList").appendChild(newItem)
+})
+
+// example :5
+
+document
+.getElementById("removeLastTask")
+.addEventListener("click", function(){
+    let taskList = document.getElementById("taskList");
+    taskList.lastElementChild.remove()
     
-// })
+    
+})
 
+
+// example : 6 -> 
+document
+.getElementById("clickMeButton")
+.addEventListener("dblclick", function(){
+    alert("Bas ker bhai ab ")
+
+})
+
+
+// example : 7 =>
+
+document
+.getElementById("teaList")
+.addEventListener("click", function(event){
+    if(event.target && event.target.matches('.teaItem')){
+        alert("you selected : " + event.target.textContent)
+    }
+})
